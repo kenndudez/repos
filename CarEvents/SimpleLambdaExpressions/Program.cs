b54d98c647838fbfb6db69de231f2b521c82ac06
kolosunde <kolosunde@sbsc.com>
2020-03-04 12:30:31 +0100

    Update

--- CarEvents/SimpleLambdaExpressions/Program.cs
+++ CarEvents/SimpleLambdaExpressions/Program.cs
@@ -12,6 +12,7 @@ namespace SimpleLambdaExpressions
         {
             Console.WriteLine("***** Fun with Lambdas *****\n");
             TraditionalDelegateSyntax();
+            AnonymousMethodSyntax();
             Console.ReadLine();
         }
 
@@ -25,6 +26,37 @@ namespace SimpleLambdaExpressions
             // Call FindAll() using traditional delegate syntax.
             Predicate<int> callback = IsEvenNumber;
             List<int> evenNumbers = list.FindAll(callback);
+            Console.WriteLine("Here are your even numbers:");
+            foreach (int evenNumber in evenNumbers)
+            {
+                Console.Write("{0}\t", evenNumber);
+            }
+            Console.WriteLine();
+        }
+
+        static void AnonymousMethodSyntax()
+        {
+            // Make a list of integers.
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+            // Now, use an anonymous method.
+            List<int> evenNumbers = list.FindAll(delegate (int i)
+            { return (i % 2) == 0; });
+            Console.WriteLine("Here are your even numbers:");
+            foreach (int evenNumber in evenNumbers)
+            {
+                Console.Write("{0}\t", evenNumber);
+            }
+            Console.WriteLine();
+        }
+
+        static void LambdaExpressionSyntax()
+        {
+            // Make a list of integers.
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+            // Now, use a C# lambda expression.
+            List<int> evenNumbers = list.FindAll(i => (i % 2) == 0);
             Console.WriteLine("Here are your even numbers:");
             foreach (int evenNumber in evenNumbers)
             {
